5793c360823bd5a04c5d17673a77fe21232bed9f
P Leon <pabloleonpsico@gmail.com>
2017-03-17 18:33:09 +0100

    Update TestJuegos.java
    Corrección de comentarios
    Extracción del método givePoints() que asigna la puntuación de los participantes en la lista de jugadores

--- sieteYmedia/TestJuegos.java
+++ sieteYmedia/TestJuegos.java
@@ -1,4 +1,4 @@
-ppackage sieteYmedia;
+package sieteYmedia;
 
 import java.util.*;
 
@@ -16,10 +16,11 @@ public class TestJuegos {
 	private static Partida partida;
 	private static Menu menu = new Menu("Siete y Media", new String[] {
 			"jugar", "mostrar ranking" });
-	
+
 	public static void main(String[] args) {
-		//los jugadores se definen al inicio del programa y se mantienen hasta el final
-		//en este caso no se ha dado opcion a introducir o eliminar jugadores.
+		// los jugadores se definen al inicio del programa y se mantienen hasta
+		// el final
+		// en este caso no se ha dado opcion a introducir o eliminar jugadores.
 		generateAllPlayers();
 		do {
 			switch (menu.gestionar()) {
@@ -46,28 +47,38 @@ public class TestJuegos {
 	 */
 	private static void ranking() {
 		Collections.sort(jugadores);
-		
+
 		for (Jugador jugador : jugadores) {
 			System.out.println(jugador);
 		}
-		}
+	}
 
 	/**
 	 * juega tres rondas con los jugadores que elija
-	 * @throws MazoVacioException 
+	 * 
+	 * @throws MazoVacioException
 	 */
 	private static void playRounds() throws MazoVacioException {
-		//al inicio de cada partida introducimos los participantes
+		// al inicio de cada partida introducimos los participantes
 		generateActualPlayers();
 		int rounds;
 		do {
-			rounds = Teclado
-					.leerEntero("por favor, indica numero de rondas");
-		} while (rounds<1);
-		for (int i = 0; i <rounds ; i++) {
+			rounds = Teclado.leerEntero("por favor, indica numero de rondas");
+		} while (rounds < 1);
+		for (int i = 0; i < rounds; i++) {
 			System.out.println("ronda " + (i + 1));
 			partida.ronda();
-		}//reasignamos el numero de partidas jugadas y ganadas
+		}// reasignamos el numero de partidas jugadas y ganadas
+		givePoints();// vaciamos la lista de participantes cuando termina la
+						// partida
+		participantes = null;
+	}
+
+	/**
+	 * asigna la puntuación de los participantes de cada ronda a la lista
+	 * general de jugadores
+	 */
+	private static void givePoints() {
 		for (Jugador j : jugadores) {
 			for (Jugador p : participantes) {
 				if (j.equals(p)) {
@@ -75,8 +86,7 @@ public class TestJuegos {
 					j.setPartidasGanadas(p.getPartidasGanadas());
 				}
 			}
-		}//vaciamos la lista de participantes cuando termina la partida
-		participantes = null;
+		}
 	}
 
 	/**
@@ -87,7 +97,8 @@ public class TestJuegos {
 		int totalJugadores = Teclado
 				.leerEntero("indica total de jugadores, recuerda que no tienen por que jugar todos a la vez");
 		for (int i = 0; i < totalJugadores; i++) {
-			//podríamos solicitar nombre del jugador, pero se ha automatizado para agilizar las pruebas
+			// podríamos solicitar nombre del jugador, pero se ha automatizado
+			// para agilizar las pruebas
 			jugadores.add(new Jugador("jugador" + (i + 1)));
 		}
 	}
@@ -103,16 +114,15 @@ public class TestJuegos {
 				opcion = Teclado
 						.leerEntero("indica que jugadores van a participar en esta ronda (pulsa 0 o "
 								+ (jugadores.size() + 1) + " para salir)");
-				if(!participantes.contains(jugadores.get(opcion-1)))
-				participantes.add(jugadores.get(opcion - 1));
-			} while (opcion > 0 || opcion > jugadores.size());
-		} catch (RuntimeException e) {
-			System.out.println("saliendo");
+				// Si ya está el jugador, no dejamos que vuelva a introducirlo
+				if (!participantes.contains(jugadores.get(opcion - 1)))
+					participantes.add(jugadores.get(opcion - 1));
+			} while (opcion < 0 || opcion < jugadores.size());
+		} catch (IndexOutOfBoundsException e) {
+			System.out.println("vamos a jugar");
 		}
 		System.out.println(participantes.toString());
 		partida = new Partida(participantes);
 	}
 
-	
 }
-
